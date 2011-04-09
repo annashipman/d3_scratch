4be413a0b48f6ff19fb12bb10fa2125890030078
Anna Shipman <annashipman@hotmail.co.uk>
2011-04-09 14:56:07 +0100

    we know nothing

--- barchart.js
+++ barchart.js
@@ -1,13 +1,19 @@
 var data = [4, 8, 15, 16, 23, 42];
 
+var x = d3.scale.linear()
+     .domain([0, d3.max(data)])
+     .range([0, 420]);
+
 var chart = d3.select("body")
     .append("svg:svg")
-    .attr("class", "chart")
+    .attr("class", "joe")
     .attr("width", 420)
     .attr("height", 20 * data.length);
 
-chart.selectAll("div")
+chart.selectAll("rect")
     .data(data)
-    .enter().append("div")
-    .style("width", function(d) { return d * 10 + "px"; })
-    .text(function(d) { return d; });
+    .enter()
+    .append("svg:rect")
+    .attr("y", function(d, i) { return i * 20; })
+    .attr("width", x)
+    .attr("height", 20);
